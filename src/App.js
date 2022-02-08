fa89564b70cc46cb28b394ce8c766ce01f7d5895
Gopala <guptaraja727@gmail.com>
2022-02-08 22:03:15 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@
 import { useState } from 'react';
 import './App.css';
 import { Button } from './components/Button';
+import {Flex} from './components/flex'
 
 function App() {
   const [theme, setTheme] = useState("light")
+  console.log(theme);
   return (
     <div className="App">
       <Button theme = {theme}>CLick me</Button>
+      <Flex>
+        <div>one</div>
+        <div>two</div>
+        <div>three</div>
+      </Flex>
       <button onClick={()=>{
-setTheme(theme === "light"? "dark":"light")
+         setTheme(theme === "light"? "dark":"light")
       }}>
         Change theme
       </button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,30 @@
-export const Button = ()=>{
-    return
-}
+import styled from "styled-components";
+
+// export const Button = ({ theme, children }) => {
+//   return (
+//     <div>
+//       <button
+//         style={{
+//           color: "coral",
+//           height: "40px",
+//           width: "100px",
+//           background: "teal",
+//           border: "0px",
+//           borderRadius: "10px",
+//           marginTop: "20px",
+//         }}
+//       >
+//         {children}
+//       </button>
+//     </div>
+//   );
+// };
+
+export const Button  = styled.button`
+          color: coral;
+          height: 40px;
+          width: 100px;
+          background: teal;
+          border: 0px;
+          border-radius: 10px;
+          marginTop: 20px,`;
